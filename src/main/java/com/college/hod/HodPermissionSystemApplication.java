a2d4c82485c2f893fae901201aee81ec285fddc3
nanda kishore <nandakishore24951a05l9@gmail.com>
2026-03-31 23:08:52 +0530

    Initial commit

---
+++ src/main/java/com/college/hod/HodPermissionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.college.hod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodPermissionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodPermissionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/hod/HodPermissionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.hod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodPermissionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
